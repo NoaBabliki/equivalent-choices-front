9b6e95891efdc0e3ee21f4276a7aad70ed3edb9d
NoaBabliki <51087030+NoaBabliki@users.noreply.github.com>
2022-01-01 14:33:48 +0200

    Add files via upload

--- src/components/DialogBox.js
+++ src/components/DialogBox.js
@@ -21,19 +21,13 @@ const DialogBox = (props) => {
             <label className='option-title'>{QUESTION}</label>
             <div>
                 <input type="radio" checked={answer === 1} className="radio-option" onChange={()=>{onChange(1)}}/> 
-                <label className="radio-title"> 
-                {<button className='button-dialog' onClick={()=> {onChange(1)}}>{OPTION_1}</button>} 
-                </label>
+                <button className='button-dialog' onClick={()=> {onChange(1)}}>{OPTION_1}</button>
                 <div className='under'></div>
                 <input type="radio" checked={answer === 2} className="radio-option" onChange={()=>{onChange(2)}}/>
-                <label className="radio-title"> 
-                {<button className='button-dialog' onClick={()=> {onChange(2)}}>{OPTION_2}</button>} 
-                </label>
+                <button className='button-dialog' onClick={()=> {onChange(2)}}>{OPTION_2}</button>
                 <div className='under'></div>
                 <input type="radio" checked={answer === 3} className="radio-option" onChange={()=>{onChange(3)}}/>
-                <label className="radio-title"> 
-                {<button className='button-dialog' onClick={()=> {onChange(3)}}>{OPTION_3}</button>} 
-                </label>
+                <button className='button-dialog' onClick={()=> {onChange(3)}}>{OPTION_3}</button>
             </div>
             <button className='confirm-button' disabled={disable} onClick={()=>{props.save_answer(answer)}}>Confirm</button>
         </div>
